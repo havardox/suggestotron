fe2388fd45cbfd0887cefd445999c4b3117858af
Harald <harald.varner@gmail.com>
2018-05-22 14:43:55 +0300

    Initial commit

---
+++ public/TopicData.php
@@ -0,0 +1,44 @@
+<?php
+class TopicData {
+
+    protected $connection = null;
+	
+	public function __construct()
+	{
+		$this->connect();
+	}
+
+    public function connect()
+    {
+        $this->connection = new PDO("mysql:host=localhost;dbname=suggestotron", "root", null);
+    }
+
+    public function getAllTopics()
+    {
+        $query = $this->connection->prepare("SELECT * FROM topics");
+        $query->execute();
+
+        return $query;
+    }
+	
+	public function add($data)
+	{
+		$query = $this->connection->prepare(
+			"INSERT INTO topics (
+				title,
+				description
+			) VALUES (
+				:title,
+				:description
+			)"
+		);
+
+		$data = [
+			':title' => $data['title'],
+			':description' => $data['description']
+		];
+
+		$query->execute($data);
+	}
+}
+?>
